c6339823ad6165658b88858572e08e9d1e8af901
BT_3W2_04 <vincentl105@nycstudents.net>
2022-11-10 08:37:17 -0500

    second question

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main (String [] args) {
+        int x = 30;
+        int y = 10;
+        int z = 5;
+        System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+
+        while ((y != 0) && (x / y >= 0)) {
+            x += 2;
+            y -= 2;
+            if (x % z != 0) {
+                z--;
+            }
+            System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+        }
+    }
 }
